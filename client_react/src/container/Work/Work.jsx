e9ec74479fba8d81674cb26c230ddbda5458fd55
Michael G. Padin <75446003+moklo0821@users.noreply.github.com>
2022-05-14 04:40:17 +0800

    create skills section

--- client_react/src/container/Work/Work.jsx
+++ client_react/src/container/Work/Work.jsx
@@ -24,6 +24,17 @@ const Work = () => {
 
   const handleWorkFilter = (item) => {
     setActiveFilter(item); 
+    setAnimateCard([{y: 100, opacity: 0}])
+
+    setTimeout(() => {
+      setAnimateCard([{y: 0, opacity: 1}]);
+
+      if(item === "All") {
+        setFilterWorks(works);
+      }else {
+        setFilterWorks(works.filter((work) => work.tags.includes(item) ))
+      }
+    }, 500)
   };
 
   return ( 
@@ -66,7 +77,7 @@ const Work = () => {
               >
                 <a href={work.projectLink} target="_blank" rel="noreferrer">
                   <motion.div
-                    whileInView={{ scale: [0, 1] }}
+                    whileInView={{ scale: [0.9, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
                     transition={{
                       duration: 0.25,
@@ -80,7 +91,7 @@ const Work = () => {
                 </a>
                 <a href={work.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
-                    whileInView={{ scale: [0, 1] }}
+                    whileInView={{ scale: [0.9, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
                     transition={{
                       duration: 0.25,
