51ef2ff9590c7ba703e5638093f9e537ccad9c6c
unknown <padinmichael201@gmail.com>
2022-07-28 02:25:50 +0800

    commit

--- client_react/src/container/Footer/Footer.jsx
+++ client_react/src/container/Footer/Footer.jsx
@@ -1,11 +1,115 @@
-import React from 'react'
+import React, { useState } from "react";
+import { client } from "../../client.js";
 
+import { images } from "../../constants";
+import { AppWrap, MotionWrap } from "../../wrapper";
+// import { client } from "../../client";
 
-import './Footer.scss';
+import "./Footer.scss";
+
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [isFormSubmitted, setIsForSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-export default Footer
+  const { name, email, message } = formData;
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    setLoading(true);
+
+    const contact = {
+      _type: "contact",
+      name: name,
+      email: email,
+      message: message,
+    };
+
+    client.create(contact).then(() => {
+      setLoading(false);
+      setIsForSubmitted(true);
+      setTimeout(() => {
+        setIsForSubmitted(false);
+        setFormData({ name: "", email: "", message: "" });
+      }, 7000);
+    });
+  };
+
+  return (
+    <>
+      <h2 className="head-text">Take a coffee & chat with me</h2>
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <img src={images.email} alt="email" />
+          <a href="mailto:padinmichael201@gmail.com" className="p-text">
+            padinmichael201@gmail.com
+          </a>
+        </div>
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="mobile" />
+          <a href="tel: +639089875407" className="p-text">
+            +63 908 987 5407
+          </a>
+        </div>
+      </div>
+
+      {!isFormSubmitted ? (
+        <div className="app__footer-form app__flex">
+          <div className="app__flex">
+            <input
+              className="p-text"
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              value={name}
+              onChange={handleChangeInput}
+            />
+          </div>
+          <div className="app__flex">
+            <input
+              className="p-text"
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              value={email}
+              onChange={handleChangeInput}
+            />
+          </div>
+
+          <div>
+            <textarea
+              className="p-text"
+              placeholder="Your Message"
+              value={message}
+              name="message"
+              onChange={handleChangeInput}
+            />
+          </div>
+
+          <button type="button" className="p-text" onClick={handleSubmit}>
+            {loading ? "Sending" : "Send Message"}
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h3 className="head-text"> Thank you for getting in touch! </h3>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(Footer, "app__footer"),
+  "contact",
+  "app__whitebg"
+);
--- client_react/src/container/Testimonials/Testimonials.jsx
+++ client_react/src/container/Testimonials/Testimonials.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+// import { motion } from "framer-motion";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 
-import './Testimonials.scss';
+import { urlFor, client } from "../../client";
+import { AppWrap, MotionWrap } from "../../wrapper";
+
+import "./Testimonials.scss";
 
 const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
-  )
-}
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-export default Testimonials
+  const handleClick = (index) => {
+    setCurrentIndex(index);
+   
+  }
+
+  useEffect(() => {
+    const testimonialsQuery = '*[_type == "testimonials"]';
+    
+    client.fetch(testimonialsQuery).then((data) => {
+      setTestimonials(data);
+    });
+  }, []);
+
+  const test = testimonials[currentIndex];
+
+  return (
+    <>
+      {testimonials.length && (
+        <>
+          <div className="app__testimonial-item app__flex">
+            <img src={urlFor(test.imgurl)} alt="testimonial" />
+            <div className="app__testimonial-content">
+              <p className="p-text">{test.feedback}</p>
+              <div>
+                <h4 className="bold-text">{test.name}</h4>
+                <h5 className="p-text">{test.company}</h5>
+              </div>
+            </div>
+            </div>
+
+            <div className="app__testimonial-btns app__flex">
+              <div className="app__flex" onClick={() => handleClick (currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
+                <HiChevronLeft />
+              </div>
+              <div className="app__flex" onClick={() => handleClick (currentIndex ===  testimonials.length - 1 ?  0: currentIndex +  1)}>
+                <HiChevronRight  />
+              </div>
+            </div>
+        </>
+      )}
+      
+  
+    </>
+  );
+};
+export default AppWrap(
+  MotionWrap(Testimonials, "app__testimonial"),
+  "testimonial",
+  "app__primarybg"
+);
