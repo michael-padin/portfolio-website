f05a868f48d381cb8cca340ac10d18b9e2b66e16
Your Name <padinmichael201@gmail.com>
2022-08-13 02:24:59 +0800

    attributes updated on social media links

--- client_react/src/components/SocialMedia.jsx
+++ client_react/src/components/SocialMedia.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import { BsInstagram, BsGithub, BsLinkedin} from "react-icons/bs";
+import {  BsGithub, BsLinkedin} from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
 
 const SocialMedia = () => {
   return (
     <div className="app__social" style={{ zIndex: "999" }}>
       <div>
-        <a href="https://github.com/michael-padin" target="_blank">
+        <a href="https://github.com/michael-padin" target="_blank" rel="noopener noreferrer">
           <BsGithub />
         </a>
       </div>
       <div>
-        <a href = "https://www.linkedin.com/in/michael-padin-96b758168" target = "_blank">
+        <a href = "https://www.linkedin.com/in/michael-padin-96b758168" target = "_blank" rel="noopener noreferrer">
         <BsLinkedin />
         </a>
       </div>
       <div>
-        <a href="https://facebook.com/mokiiiiieeeee" target = "_blank">
+        <a href="https://facebook.com/mokiiiiieeeee" target = "_blank" rel="noopener noreferrer">
           <FaFacebookF />
         </a>
       </div>
