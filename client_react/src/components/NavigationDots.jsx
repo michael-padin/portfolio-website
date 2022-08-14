ebd3bf11234f9651d910d4712d7656a00d93db7c
Your Name <padinmichael201@gmail.com>
2022-08-15 07:30:38 +0800

    change active background color

--- client_react/src/components/NavigationDots.jsx
+++ client_react/src/components/NavigationDots.jsx
@@ -10,7 +10,7 @@ const NavigationDots = ({ active }) => {
           href={`#${item}`}
           key={item + index}
           className="app__navigation-dot"
-          style={active === item ? { backgroundColor: "#313bac" } : {}}
+          style={active === item ? { backgroundColor: "#2190ff" } : {}}
         />
       ))}
     </div>
