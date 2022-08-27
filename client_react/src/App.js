d925428796eef3d249491d362ca034227a717800
Your Name <padinmichael201@gmail.com>
2022-08-27 15:43:21 +0800

    Update: Added react helmet

--- client_react/src/App.js
+++ client_react/src/App.js
@@ -1,8 +1,11 @@
-import React, { lazy } from "react";
+import React, { lazy} from "react";
 
 import { Navbar } from "./components";
-import "./App.scss";
 import { Suspense } from "react";
+import {images} from "./constants"
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+
+import "./App.scss";
 
 const Header = lazy(() => import("./container/Header/Header"));
   const About = lazy(() => import("./container/About/About"));
@@ -14,9 +17,23 @@ const Header = lazy(() => import("./container/Header/Header"));
   const Footer = lazy(() => import("./container/Footer/Footer"));
 
 const App = () => {
-  
+
   return (
-    <Suspense fallback={<div></div>}>
+    <HelmetProvider>
+       <Helmet>
+        <meta name="description" content="Solution-oriented and problem solver, experience building web applications using MERN stack technologies. Have done numerous projects involving frontend and backend development"/>
+        <meta name="keywords" content="fullstack, nodejs, reactjs, develoeper, michaelpadin, JavaScript,"/>
+        <meta name="author" content="Michael Padin" />
+        <meta itemprop="description" content="Solution-oriented and problem solver, experience building web applications using MERN stack technologies. Have done numerous projects involving frontend and backend development"/>
+        <meta itemprop="image" content={images.heroSection}/>
+        <meta itemprop="name" content="Michael Padin - Fullstack Developer" />
+        <meta property="og:image" content={images.heroSection} />
+        <meta property="og:url" content="https://michaelpadin.netlify.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Michael Padin - Fullstack Developer" />
+        <meta property="og:description" content="Solution-oriented and problem solver, experience building web applications using MERN stack technologies. Have done numerous projects involving frontend and backend development"/>
+      </Helmet>
+      <Suspense fallback = {<div></div>}>
       <div className="app">
         <Navbar />
         <Header />
@@ -26,7 +43,8 @@ const App = () => {
         <Testimonials />
         <Footer />
       </div>
-    </Suspense>
+      </Suspense>
+      </HelmetProvider>
   );
 };
 
