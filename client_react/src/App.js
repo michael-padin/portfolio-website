91d3f295d45aedcbae2ffee8f680bb5464e5305d
Your Name <padinmichael201@gmail.com>
2022-08-28 06:00:39 +0800

    Update: Removed react-helmet

--- client_react/src/App.js
+++ client_react/src/App.js
@@ -2,7 +2,6 @@ import React, { lazy} from "react";
 
 import { Navbar } from "./components";
 import { Suspense } from "react";
-import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 import "./App.scss";
 
@@ -18,26 +17,6 @@ const Header = lazy(() => import("./container/Header/Header"));
 const App = () => {
 
   return (
-    <HelmetProvider>
-       <Helmet>
-        <meta name="description" content="Solution-oriented and problem solver, experience building web applications using MERN stack technologies."/>
-        <meta name="keywords" content="fullstack, nodejs, reactjs, develoeper, michaelpadin, JavaScript,"/>
-        <meta name="author" content="Michael Padin" />
-        <meta itemprop="description" content="Solution-oriented and problem solver. Experience building web applications using MERN stack technologies."/>
-        <meta itemprop="image" content="https://i.ibb.co/940mgJy/hero-section.webp"/>
-        <meta itemprop="name" content="Michael Padin - Fullstack Developer" />
-        <meta property="og:image" content="https://i.ibb.co/940mgJy/hero-section.webp" />
-        <meta property="og:image:width" content="1200"/>
-        <meta property="og:image:height" content="630"/>
-        <meta property="og:image:secure_url" content="https://secure.i.ibb.co/940mgJy/hero-section.webp" />
-        <meta property="og:image:type" content="image/webp" />
-        <meta property="og:image:alt" content="Michael Padin portfolio website" />
-        <meta property="og:url" content="https://michaelpadin.netlify.app/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Michael Padin - Fullstack Developer" />
-        <meta property="og:description" content="Solution-oriented and problem solver. Experience building web applications using MERN stack technologies."/>
-        <link rel="canonical" href="https://michaelpadin.netlify.app/" />
-      </Helmet>
       <Suspense fallback = {<div></div>}>
       <div className="app">
         <Navbar />
@@ -49,7 +28,6 @@ const App = () => {
         <Footer />
       </div>
       </Suspense>
-      </HelmetProvider>
   );
 };
 
