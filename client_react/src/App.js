dfaceabf46ebfb6270ad35fcb61794e965ae5ea9
Your Name <padinmichael201@gmail.com>
2022-08-27 17:06:48 +0800

    Update: Added react helmet

--- client_react/src/App.js
+++ client_react/src/App.js
@@ -2,7 +2,6 @@ import React, { lazy} from "react";
 
 import { Navbar } from "./components";
 import { Suspense } from "react";
-import {images} from "./constants"
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 import "./App.scss";
@@ -21,17 +20,18 @@ const App = () => {
   return (
     <HelmetProvider>
        <Helmet>
-        <meta name="description" content="Solution-oriented and problem solver, experience building web applications using MERN stack technologies. Have done numerous projects involving frontend and backend development"/>
+        <meta name="description" content="Solution-oriented and problem solver, experience building web applications using MERN stack technologies."/>
         <meta name="keywords" content="fullstack, nodejs, reactjs, develoeper, michaelpadin, JavaScript,"/>
         <meta name="author" content="Michael Padin" />
-        <meta itemprop="description" content="Solution-oriented and problem solver, experience building web applications using MERN stack technologies. Have done numerous projects involving frontend and backend development"/>
-        <meta itemprop="image" content={images.heroSection}/>
+        <meta itemprop="description" content="Solution-oriented and problem solver, experience building web applications using MERN stack technologies."/>
+        <meta itemprop="image" content="https://i.ibb.co/940mgJy/hero-section.webp"/>
         <meta itemprop="name" content="Michael Padin - Fullstack Developer" />
-        <meta property="og:image" content={images.heroSection} />
+        <meta property="og:image" content="https://i.ibb.co/940mgJy/hero-section.webp" />
         <meta property="og:url" content="https://michaelpadin.netlify.app/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Michael Padin - Fullstack Developer" />
-        <meta property="og:description" content="Solution-oriented and problem solver, experience building web applications using MERN stack technologies. Have done numerous projects involving frontend and backend development"/>
+        <meta property="og:description" content="Solution-oriented and problem solver, experience building web applications using MERN stack technologies."/>
+        <link rel="canonical" href="https://michaelpadin.netlify.app/" />
       </Helmet>
       <Suspense fallback = {<div></div>}>
       <div className="app">
