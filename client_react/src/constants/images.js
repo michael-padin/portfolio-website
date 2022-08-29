d3cbea54c72ab5671382917aac6babd4ff6b6bf8
Your Name <padinmichael201@gmail.com>
2022-08-29 21:43:33 +0800

    update: removed unused static images

--- client_react/src/constants/images.js
+++ client_react/src/constants/images.js
@@ -1,19 +1,6 @@
 // @ts-nocheck
 import email from '../assets/email.png';
 import mobile from '../assets/mobile.png';
-import api from '../assets/api.png';
-import css from '../assets/css.png';
-import figma from '../assets/figma.png';
-import git from '../assets/git.png';
-import graphql from '../assets/graphql.png';
-import html from '../assets/html.png';
-import javascript from '../assets/javascript.png';
-import mu5 from '../assets/mu5.png';
-import node from '../assets/node.png';
-import react from '../assets/react.png';
-import redux from '../assets/redux.png';
-import sass from '../assets/sass.png';
-import typescript from '../assets/typescript.png';
 import me from "../assets/me.webp"
 import logo from '../assets/logo.png';
 
@@ -21,19 +8,6 @@ const images = {
   me,
   email,
   mobile,
-  api,
-  css,
-  figma,
-  git,
-  graphql,
-  html,
-  javascript,
-  mu5,
-  node,
-  react,
-  redux,
-  sass,
-  typescript,
   logo,
 }
 
