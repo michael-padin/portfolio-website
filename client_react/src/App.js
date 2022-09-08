88e4297fee004188c80c3ab4afef3aeace93f114
Your Name <padinmichael201@gmail.com>
2022-09-08 22:35:05 +0800

    refactor: remove socialIcons and navdots to App

--- client_react/src/App.js
+++ client_react/src/App.js
@@ -1,33 +1,36 @@
-import React, { lazy} from "react";
+import React, { lazy } from "react";
 
-import { Navbar } from "./components";
+import { Navbar, SocialMedia, NavigationDots } from "./components";
 import { Suspense } from "react";
 
 import "./App.scss";
 
 const Header = lazy(() => import("./container/Header/Header"));
-  const About = lazy(() => import("./container/About/About"));
-  const Work = lazy(() => import("./container/Work/Work"));
-  const Skills = lazy(() => import("./container/Skills/Skills"));
-  const Testimonials = lazy(() =>
-    import("./container/Testimonials/Testimonials")
-  );
-  const Footer = lazy(() => import("./container/Footer/Footer"));
+const About = lazy(() => import("./container/About/About"));
+const Work = lazy(() => import("./container/Work/Work"));
+const Skills = lazy(() => import("./container/Skills/Skills"));
+const Testimonials = lazy(() =>
+  import("./container/Testimonials/Testimonials")
+);
+const Footer = lazy(() => import("./container/Footer/Footer"));
 
 const App = () => {
-
   return (
-      <Suspense fallback = {<div></div>}>
+    <Suspense fallback={<div></div>}>
       <div className="app">
-        <Navbar />
-        <Header />
-        <About />
-        <Work />
-        <Skills />
-        <Testimonials />
-        <Footer />
+        <SocialMedia />
+        <div>
+          <Navbar />
+          <Header />
+          <About />
+          <Work />
+          <Skills />
+          <Testimonials />
+          <Footer />
+        </div>
+        <NavigationDots />
       </div>
-      </Suspense>
+    </Suspense>
   );
 };
 
--- client_react/src/components/NavigationDots.jsx
+++ client_react/src/components/NavigationDots.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import { memo } from "react";
 
 const NavigationDots = ({ active }) => {
-  
   return (
     <div className="app__navigation">
-      {["home", "about", "work", "skills", "testimonial","contact"].map((item, index) => (
-        //eslint-disable-next-line
-        <a
-          href={`#${item}`}
-          key={item + index}
-          className="app__navigation-dot"
-          style={active === item ? { backgroundColor: "#2190ff" } : {}}
-        />
-      ))}
+      {["home", "about", "work", "skills", "testimonial", "contact"].map(
+        (item, index) => (
+          //eslint-disable-next-line
+          <a
+            href={`#${item}`}
+            key={item + index}
+            className="app__navigation-dot"
+            style={active === item ? { backgroundColor: "#2190ff" } : {}}
+          />
+        )
+      )}
     </div>
   );
 };
