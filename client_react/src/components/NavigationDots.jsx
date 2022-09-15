2b8bec510c878f433d92406787f6538146a235b9
Your Name <padinmichael201@gmail.com>
2022-09-15 20:09:48 +0800

    fix: active navigation dots

--- client_react/src/components/NavigationDots.jsx
+++ client_react/src/components/NavigationDots.jsx
@@ -1,18 +1,33 @@
-import React from "react";
-import { memo } from "react";
+import React, { useEffect, useState } from 'react';
+import { memo } from 'react';
 
-const NavigationDots = ({ active }) => {
+const NavigationDots = () => {
+  const [hashLocation, setHashLocation] = useState('');
+
+  const handleClick = () => {
+    // use setTimeout to wait for the exact location before getting the hash location
+    setTimeout(() => {
+      setHashLocation(window.location.hash.replace("#", ""));
+    }, 100);
+  };
+
+  // if hash location is empty we set initial valu to 'home'
+  useEffect(() => {
+    const location = window.location.hash.replace("#", "");
+    setHashLocation(location !== '' ? location : 'home');
+  }, [])
+
   return (
     <div className="app__navigation">
-      {["home", "about", "work", "skills", "testimonial", "contact"].map(
+      {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map(
         (item, index) => (
-          //eslint-disable-next-line
-          <a
-            href={`#${item}`}
-            key={item + index}
-            className="app__navigation-dot"
-            style={active === item ? { backgroundColor: "#2190ff" } : {}}
-          />
+          <a href={`#${item}`} key={item + index}>
+            <div
+              className="app__navigation-dot"
+              style={hashLocation === item ? { backgroundColor: '#2190ff' } : {}}
+              onClick={handleClick}
+            />
+          </a>
         )
       )}
     </div>
